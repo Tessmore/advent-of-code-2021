3e18de440515a272b97ce6466329b6ba5e2cfcae
Fabien Tesselaar <fabientesselaar@gmail.com>
2021-12-07 19:51:24 +0100

    Day05

--- day05/main.py
+++ day05/main.py
@@ -1,14 +1,18 @@
 
-class Lines:
-    x1 = 0
-    x2 = 0
+class Line:
+    nr = 0
 
+    x1 = 0
     y1 = 0
+
+    x2 = 0
     y2 = 0
 
-    dots = []
+    slope = None
 
-    def __init__(self):
+    def __init__(self, nr):
+        self.nr = nr
+
         (x, y)= input().split("->")
         (x1, y1) = [int(i) for i in x.split(",")]
         (x2, y2) = [int(i) for i in y.split(",")]
@@ -19,28 +23,59 @@ class Lines:
         self.y1 = y1
         self.y2 = y2
 
+        if  (x2 - x1) > 0:
+            self.slope = (y2 - y1) / (x2 - x1)
+
     def is_vertical(self):
         return self.x1 == self.x2
 
     def is_horizontal(self):
         return self.y1 == self.y2
 
+    def overlap(self, other):
+        print("CHECKING")
+        self.show()
+        other.show()
+
+        return 0
+
     def show(self):
-        print(self.x1,self.y1, "->", self.x2,self.y2)
+        print(self.x1, self.y1, "->", self.x2, self.y2)
 
 
 lines = []
 
-max_x = 0
-max_y = 0
+nr = 0
 
 while True:
     try:
-        tmp = Lines()
+        tmp = Line(nr)
 
-        # part 1: Only horizontal lines
+        # Part 1: Only horizontal lines
         if tmp.is_horizontal() or tmp.is_vertical():
             lines.append(tmp)
+            nr += 1
 
     except EOFError:
         break
+
+print("LEN", len(lines))
+
+for a in lines:
+    for b in lines:
+        # Ignore comparing a line against itself
+        # Ignore lines with a different slope (part 1)
+        if a.nr <= b.nr:
+            continue
+
+        if a.is_vertical() and not b.is_vertical():
+            continue
+
+        if a.is_horizontal() and not b.is_horizontal():
+            continue
+
+        print(a.overlap(b))
+        print("---")
+
+        # break
+
